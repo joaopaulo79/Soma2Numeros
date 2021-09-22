b2dfa7c2f66839064b3f5758360e3b478336b2d2
João Paulo <joaopdejesus79@gmail.com>
2021-09-22 10:47:51 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("==== Soma de dois Números ====");
+            Console.WriteLine();
+            Console.Write("Digite um número: ");
+            Console.ResetColor();
+            string aBase = Console.ReadLine();
+            double a = Convert.ToDouble(aBase);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Digite outro número: ");
+            Console.ResetColor();
+            string bBase = Console.ReadLine();
+            double b = Convert.ToDouble(bBase);
+            double soma = a + b;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("== Resultado ==");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine($"{a} + {b} = {soma}");
+            Console.WriteLine();
+        }
+    }
+}
