929ed5a7a6bc9cf7ac97aff66390b5d3fcd002df
João Paulo <joaopdejesus79@gmail.com>
2021-09-25 10:00:33 -0300

    Programa Finalizado!

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ namespace Soma2Numeros
         {
             int a, b, soma;
             
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("\n===== Soma de dois números Inteiros =====\n");
+            Console.ResetColor();
+
             Console.WriteLine("Digite os números a serem somados, um em");
             Console.WriteLine("cada linha.\n");
 
@@ -20,32 +23,11 @@ namespace Soma2Numeros
 
             soma = a + b;
 
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("\n=== Resultado ===\n");
+            Console.ResetColor();
+
             Console.WriteLine($"{a} + {b} = {soma}\n");
-            
-            //Console.WriteLine();
-            //Console.ForegroundColor = ConsoleColor.DarkYellow;
-            //Console.WriteLine("==== Soma de dois Números ====");
-            //Console.WriteLine();
-            //Console.Write("Digite um número: ");
-            //Console.ResetColor();
-            //string aBase = Console.ReadLine();
-            //double a = Convert.ToDouble(aBase);
-            //Console.WriteLine();
-            //Console.ForegroundColor = ConsoleColor.DarkYellow;
-            //Console.Write("Digite outro número: ");
-            //Console.ResetColor();
-            //string bBase = Console.ReadLine();
-            //double b = Convert.ToDouble(bBase);
-            //double soma = a + b;
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.ForegroundColor = ConsoleColor.DarkYellow;
-            //Console.WriteLine("== Resultado ==");
-            //Console.ResetColor();
-            //Console.WriteLine();
-            //Console.WriteLine($"{a} + {b} = {soma}");
-            //Console.WriteLine();
         }
     }
 }
